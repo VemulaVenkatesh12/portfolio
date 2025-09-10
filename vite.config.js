787b1183ca95c345ef301c49f91d2abae7bfc56b
vemula.venkatesh@in.nspglobaltech.com <vemulavenky08@gmail.com>
2025-09-10 13:54:04 +0530

    ...

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/',       // Use '/portfolio/' if your repo is not root
+  base: '/portfolio/', // Set this to your repository name
   plugins: [react()],
-})
+});
